33133c1fb4bcfec47ec1416442c57e790560cc3e
Stefan-Andrei Florea <stefflr@yahoo.com>
2020-09-29 10:18:54 +0200

    updated

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let movies = [];
 let bullets = [];
 const Movie = {
   title: "",
+  titleDk: "",
   year: 0,
   length: "",
   director: "",
@@ -44,10 +45,12 @@ function prepareObjectMovie(jsonObject) {
   const movie = Object.create(Movie);
 
   movie.title = jsonObject.title.original;
+  movie.titleDk = jsonObject.title.danish;
   //getDK
   movie.year = jsonObject.year;
   movie.length = jsonObject.length;
-  movie.writers = jsonObject.writers.novel;
+  movie.writers = jsonObject.writers.screenplay;
+  movie.director = jsonObject.director;
   //getScreenplay
   movie.poster = "./images/" + jsonObject.poster;
 
@@ -65,14 +68,7 @@ function addInfoBoxEvents() {
   createInfobox();
 }
 
-function createInfobox(i) {
-  //   let infoboxTexts = document.querySelector("#infobox_x5F_template").querySelectorAll("text");
-  //   infoboxTexts[0].textContent = movies[i].title;
-  //   infoboxTexts[1].textContent = "caca";
-  //   infoboxTexts[2].textContent = movies[i].year;
-  //   console.log(infoboxTexts[2], movies[i].year);
-  //   console.log(infoboxTexts[0], movies[i].title);
-
+function createInfobox() {
   let newInfobox = document.createElementNS("http://www.w3.org/2000/svg", "use");
   newInfobox.setAttribute("href", "#infobox_x5F_template");
   document.querySelector("#infoboxes-timeline").appendChild(newInfobox);
@@ -82,16 +78,37 @@ function selectBullet() {
   for (let i = 0; i < bullets.length; i++) {
     if (bullets[i] == this) {
       console.log(i, movies[i]);
-      displayInfoBox(this);
+      displayInfoBox(this, movies[i]);
     }
   }
 }
 
-function displayInfoBox(bullet) {
+function displayInfoBox(bullet, movie) {
   document.querySelector("#infoline line").setAttribute("x1", bullet.getAttribute("cx"));
   document.querySelector("#infoline line").setAttribute("y1", bullet.getAttribute("cy"));
-  document.querySelector("#infoline line").setAttribute("x2", parseInt(bullet.getAttribute("cx")) + 54);
-  document.querySelector("#infoline line").setAttribute("y2", parseInt(bullet.getAttribute("cy")) - 207);
-  document.querySelector("use").setAttribute("x", parseInt(bullet.getAttribute("cx")) + 54);
-  document.querySelector("use").setAttribute("y", parseInt(bullet.getAttribute("cy")) - 207 - 302);
+
+  const bounding = document.querySelector("use").getBoundingClientRect();
+
+  if (bounding.right > (window.innerWidth || document.documentElement.clientWidth)) {
+    document.querySelector("use").setAttribute("x", parseInt(bullet.getAttribute("cx")) - 54 - 836);
+    document.querySelector("use").setAttribute("y", parseInt(bullet.getAttribute("cy")) - 207 - 302);
+
+    document.querySelector("#infoline line").setAttribute("x2", parseInt(bullet.getAttribute("cx")) - 54);
+    document.querySelector("#infoline line").setAttribute("y2", parseInt(bullet.getAttribute("cy")) - 207);
+  } else {
+    document.querySelector("use").setAttribute("x", parseInt(bullet.getAttribute("cx")) + 54);
+    document.querySelector("use").setAttribute("y", parseInt(bullet.getAttribute("cy")) - 207 - 302);
+
+    document.querySelector("#infoline line").setAttribute("x2", parseInt(bullet.getAttribute("cx")) + 54);
+    document.querySelector("#infoline line").setAttribute("y2", parseInt(bullet.getAttribute("cy")) - 207);
+  }
+
+  document.querySelectorAll("#infobox_x5F_template text")[0].textContent = movie.title;
+  document.querySelectorAll("#infobox_x5F_template text")[1].textContent = movie.titleDk;
+  document.querySelectorAll("#infobox_x5F_template text")[2].textContent = movie.year;
+  document.querySelectorAll("#infobox_x5F_template text")[3].textContent = movie.length;
+  document.querySelectorAll("#infobox_x5F_template text")[5].textContent = movie.director;
+  document.querySelectorAll("#infobox_x5F_template text")[7].textContent = movie.writers;
+  document.querySelector("#infobox_x5F_template image").setAttribute("xlink:href", movie.poster);
+
 }
